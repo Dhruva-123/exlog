8299b1a1a086b53aab9350e7e454406f7d9edc75
Dhruva-123 <rahuldhruva.k9@gmail.com>
2025-09-06 19:14:16 +0530

    setup

--- setup.py
+++ setup.py
@@ -2,6 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name='exlog',
-    version='0.1',
+    version='0.1.0',
+    author='Rahul Dhruva',
+    author_email='rahuldhruva.k9@gmail.com',
+    description='A one-line logging explainability tool for ML models',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="",
     packages=find_packages(),
+    install_requires = [
+        "numpy","pandas","scikit-learn","shap","torch","tensorflow","xgboost","lightgbm"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+    ],
+    python_requires=">=3.8",
 )
